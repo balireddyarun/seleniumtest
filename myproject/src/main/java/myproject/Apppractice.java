58ad4746cd89d5e4afd7aa3267a8c7d2770510b8
balireddyarun <balireddyarun98@gmail.com>
2023-04-05 09:52:18 +0530

    selenium test commit

---
+++ myproject/src/main/java/myproject/Apppractice.java
@@ -0,0 +1,22 @@
+package myproject;
+
+public class Apppractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		int a=10,b=15;
+		if(a>b)
+		{
+			System.out.println("true");
+		}
+		else
+		{
+			System.out.println("false");
+		}
+		
+		
+
+	}
+
+}
